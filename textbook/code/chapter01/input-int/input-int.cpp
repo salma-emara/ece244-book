415cac71b47d3983d7faada42d26466a5013c1d4
Salma Emara <salma.emara@mail.utoronto.ca>
2023-08-30 19:04:28 -0400

    Update chapter 1 code

---
+++ textbook/code/chapter01/input-int/input-int.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int value;
+  cout << "Enter a value: ";
+  cin >> value;
+  cout << "The value entered is " << value << endl;
+  cout << "Size of long " << sizeof(double);
+  return 0;
+}
