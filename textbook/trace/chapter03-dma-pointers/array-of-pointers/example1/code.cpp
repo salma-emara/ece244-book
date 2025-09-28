52552b710e1c43391a47c675422aa47718ed87f8
Salma Emara <salma.emara@mail.utoronto.ca>
2025-09-28 15:12:09 -0400

    Added trace for chapter 3

--- textbook/trace/chapter03-dma-pointers/array-of-pointers/example1/code.cpp
+++ textbook/trace/chapter03-dma-pointers/array-of-pointers/example1/code.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-  int** p2p; 
-  int* p, *q;     
+  int** p2p = NULL; 
+  int* p = NULL, *q = NULL;     
   p = new int;  
   *p = 5;
   p2p = &p ; 
   q = *p2p; 
-  *q = 8; 
+  *q = 10; 
+  **p2p = 8;
   cout << "*p = " << *p << endl; 
   cout << "*q = " << *q << endl; 
   cout << "**p2p = " << **p2p << endl; 
